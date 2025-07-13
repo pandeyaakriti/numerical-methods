5db37dffd53602ad26767cfa0f93517f41be9830
pandeyaakriti <aakriti.pandeyyy@gmail.com>
2025-07-13 22:01:43 +0545

    initial commit

---
+++ interpolation/3_finite-difference-table.py
@@ -0,0 +1,4 @@
+#construct finite difference table of f(x)=e^x on interval -1<=x<=1 by dividing interval by equally space points of step size 0.1
+
+import numpy as np
+import matplotlib.pyplot as plt
---
+++ interpolation/4_newton-interpolation.py
@@ -0,0 +1,4 @@
+# estimate value of function f(0.21) and f(0.29) applying newtons forward and backward interpolation formulae on the table 
+# x= 0.20   0.22   0.24   0.26   0.28   0.30
+# y= 1.6596 1.6698 1.6804 1.6912 1.7024 1.7139
+
